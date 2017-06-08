2129d57b2f2d0a55ad41e55fe073cd2009055b43
Jim Ryan Zulueta <jvtalan1@up.edu.ph>
2017-06-08 21:33:23 +0800

    heroku deploy

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,6 +88,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.action_mailer.default_url_options = { :host => 'https://shielded-shore-69763.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 
 end
