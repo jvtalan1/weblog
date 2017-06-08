07f036a465df3d77b2006645113eac4c104a8d95
Jim Ryan Zulueta <jvtalan1@up.edu.ph>
2017-06-08 17:48:38 +0800

    Add admin profile

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Admin.create (firstname: 'Admin', lastname: 'Istrator', username: 'admin',
+	email: 'admin@example.com', password: 'password', password_confirmation: 'password')
