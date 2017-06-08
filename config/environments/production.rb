ba6e3d4a42790fa1f137a348aa96abe89705b7fd
Jim Ryan Zulueta <jvtalan1@up.edu.ph>
2017-06-08 22:44:44 +0800

    Configure action mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,6 +88,16 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { host: 'safe-island-25707.herokuapp.com' }
+
+  ActionMailer::Base.smtp_settings = {
+    :address        => 'smtp.gmail.com',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => 'jvtalan1',
+    :password       => 'jvtalan1123456',
+    :domain         => 'heroku.com',
+    :enable_starttls_auto => true
 
 end
