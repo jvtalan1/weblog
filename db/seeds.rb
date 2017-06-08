fca5f3a89624f245ec3fa3bc227cecedf18d0245
Jim Ryan Zulueta <jvtalan1@up.edu.ph>
2017-06-08 18:51:29 +0800

    Update rake seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,5 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Admin.create ({'firstname' => 'Admin', 'lastname' => 'Istrator', 'username' => 'admin',
-	'email' => 'admin@example.com', 'password' => 'password', 'password_confirmation' => 'password'})
+Admin.create ({'firstname' => 'Jestoni	', 'lastname' => 'Talan', 'username' => 'jvtalan1',
+	'email' => 'jvtalan1@up.edu.ph', 'password' => 'password', 'password_confirmation' => 'password'})
