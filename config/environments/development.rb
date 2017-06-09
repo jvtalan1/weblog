0d48d8a44fa5af15ddec89c4b34578affd0ea6e6
Jim Ryan Zulueta <jvtalan1@up.edu.ph>
2017-06-09 10:04:24 +0800

    Config action mailer in env

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -51,5 +51,6 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  Rails.application.routes.default_url_options[:host] = 'safe-island-25707.herokuapp.com'
 
 end
--- config/environments/test.rb
+++ config/environments/test.rb
@@ -39,5 +39,6 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+  Rails.application.routes.default_url_options[:host] = 'safe-island-25707.herokuapp.com'
  
 end
