5ecb9035bc1c6b4a5e110a4aa21db87794dd2ddd
Jestoni Talan <jvtalan1@up.edu.ph>
2017-06-21 14:25:12 +0800

    Updates UI for better display

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -2,7 +2,6 @@ class Contact < MailForm::Base
   attribute :name,      :validate => true
   attribute :email,     :validate => /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
   attribute :message
-  attribute :nickname,  :captcha  => true
 
   # Declare the e-mail headers. It accepts anything the mail method
   # in ActionMailer accepts.
@@ -10,7 +9,7 @@ class Contact < MailForm::Base
     {
       :subject => "New message from rails blog",
       :to => "jvtalan1@up.edu.ph",
-      :from => %("#{name}" "#{nickname}" <#{email}>)
+      :from => %("#{name}" <#{email}>)
     }
   end
 end
